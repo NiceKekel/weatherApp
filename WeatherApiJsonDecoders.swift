d3b3c7edfa9c5468e12dc1b56f9477e1a1619b4b
NiceKekel <ivashenkojan@gmail.com>
2025-07-29 11:47:57 +0300

    Update WeatherApiJsonDecoders.swift

--- WeatherApiJsonDecoders.swift
+++ WeatherApiJsonDecoders.swift
@@ -1,29 +1,6 @@
 
 import Foundation
 
-//struct FreeWeatherData {
-//    let current: Current
-//    let hourly: Hourly
-//    let daily: Daily
-//    
-//    struct Current {
-//      let time: Date
-//      let temperature2m: Float
-//      let weatherCode: Float
-//    }
-//    struct Hourly {
-//      let time: [Date]
-//      let temperature2m: [Float]
-//      let precipitation: [Float]
-//    }
-//    struct Daily {
-//      let time: [Date]
-//      let temperature2mMax: [Float]
-//      let temperature2mMin: [Float]
-//    }
-//  }
-
-
 struct WeatherForecastData{
     struct Data: Codable {
         let location: Location
